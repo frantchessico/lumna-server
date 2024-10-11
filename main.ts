8788f9c95b8ff39d9a607b65497ef25105d03566
frantchessico <contentscreatorsp@gmail.com>
2024-10-11 12:45:50 +0200

    add docs

--- main.ts
+++ main.ts
@@ -2,10 +2,12 @@ import express, { Request, Response } from 'express';
 import mongoose, { Document, Schema } from 'mongoose';
 import { initializeApp } from 'firebase/app';
 import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
-import { getDatabase, ref as dbRef, push, set } from 'firebase/database';
-import multer, { MulterError } from 'multer'; // Importa multer para manipulação de arquivos
+import multer, { MulterError } from 'multer'; 
 import dotenv from 'dotenv';
 import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
+import * as swaggerDocument from './swagger.json';
+
 dotenv.config();
 
 interface AuthenticatedRequest extends Request {
@@ -25,15 +27,15 @@ const firebaseConfig = {
 
 const firebaseApp = initializeApp(firebaseConfig);
 const storage = getStorage(firebaseApp);
-const db = getDatabase(firebaseApp);
 
 // Inicialização do Express
 const app = express();
-app.use(cors())
+app.use(cors());
 const PORT = process.env.PORT || 3000;
 
 // Middleware para JSON
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Conexão com o MongoDB
 mongoose.connect(process.env.MONGO_URI || '', {})
@@ -51,15 +53,15 @@ enum AudioCategory {
   OTHER = 'Other',
 }
 
-// Atualizando o esquema do Mongoose
+// Esquema de Mongoose para músicas
 interface IAudio extends Document {
   title: string;
   description: string;
   artist: string;
-  artistAvatar: string; // Avatar do artista
+  artistAvatar: string;
   album: string;
   genre: string;
-  duration: number; // duração em segundos
+  duration: number;
   releaseDate: Date;
   copyright: string;
   collaborators: { role: string; name: string }[];
@@ -70,50 +72,64 @@ interface IAudio extends Document {
   createdAt: Date;
   producer: string;
   composer: string;
-  category: AudioCategory; // Adicionar categoria
+  category: AudioCategory;
+  playCount: number;
 }
 
 const audioSchema = new Schema<IAudio>({
   title: { type: String, required: true },
   description: { type: String, default: '' },
-  artist: { type: String, required: true }, // Nome do artista
-  artistAvatar: { type: String, required: true }, // URL do avatar do artista
-  album: { type: String, default: '' }, // Nome do álbum, se aplicável
-  genre: { type: String, default: '' }, // Gênero musical
-  duration: { type: Number, required: true }, // Duração da faixa (em segundos)
-  releaseDate: { type: Date, default: Date.now }, // Data de lançamento
-  copyright: { type: String, default: '' }, // Informações de direitos autorais
-  collaborators: [{ role: { type: String }, name: { type: String } }], // Ex: produtor, compositor, etc.
-  trackNumber: { type: Number, default: 1 }, // Número da faixa no álbum
-  totalTracks: { type: Number, default: 1 }, // Número total de faixas no álbum
-  url: { type: String, required: true }, // URL do arquivo de áudio
-  cover: { type: String, default: '' }, // URL da capa do álbum ou single
-  createdAt: { type: Date, default: Date.now }, // Data de criação no sistema
-  producer: { type: String, default: '' }, // Produtor da faixa
-  composer: { type: String, default: '' }, // Compositor da faixa
-  category: { type: String, enum: Object.values(AudioCategory), default: AudioCategory.OTHER } // Categoria como enum
+  artist: { type: String, required: true },
+  artistAvatar: { type: String, required: true },
+  album: { type: String, default: '' },
+  genre: { type: String, default: '' },
+  duration: { type: Number, required: true },
+  releaseDate: { type: Date, default: Date.now },
+  copyright: { type: String, default: '' },
+  collaborators: [{ role: { type: String }, name: { type: String } }],
+  trackNumber: { type: Number, default: 1 },
+  totalTracks: { type: Number, default: 1 },
+  url: { type: String, required: true },
+  cover: { type: String, default: '' },
+  createdAt: { type: Date, default: Date.now },
+  producer: { type: String, default: '' },
+  composer: { type: String, default: '' },
+  category: { type: String, enum: Object.values(AudioCategory), default: AudioCategory.OTHER },
+  playCount: { type: Number, default: 0 }
 });
 
 const Audio = mongoose.model<IAudio>('Audio', audioSchema);
 
-// Definindo o esquema de Favoritos
-interface IFavorite extends Document {
-  userId: string; // ID do usuário
-  audioId: mongoose.Types.ObjectId; // ID do áudio (ObjectId do Mongoose)
-  createdAt: Date;
-}
-
-const favoriteSchema = new Schema<IFavorite>({
-  userId: { type: String, required: true },
-  audioId: { type: Schema.Types.ObjectId, ref: 'Audio', required: true }, // Referência para o áudio
-  createdAt: { type: Date, default: Date.now }
-});
-
-const Favorite = mongoose.model<IFavorite>('Favorite', favoriteSchema);
-
 // Configurando o multer para armazenar o arquivo em memória
 const storageMulter = multer.memoryStorage();
 const upload = multer({ storage: storageMulter });
+
+// Esquema para álbuns
+interface IAlbum extends Document {
+  title: string;
+  artist: string;
+  artistAvatar: string;
+  genre: string;
+  releaseDate: Date;
+  cover: string;
+  tracks: mongoose.Types.ObjectId[];
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const albumSchema = new Schema<IAlbum>({
+  title: { type: String, required: true },
+  artist: { type: String, required: true },
+  artistAvatar: { type: String, required: true },
+  genre: { type: String, required: true },
+  releaseDate: { type: Date, default: Date.now },
+  cover: { type: String, default: '' },
+  tracks: [{ type: Schema.Types.ObjectId, ref: 'Audio' }],
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+});
+
+const Album = mongoose.model<IAlbum>('Album', albumSchema);
 
 // Interface para definir os tipos dos dados que esperamos receber no corpo da requisição
 interface UploadRequest extends Request {
@@ -133,7 +149,7 @@ interface UploadRequest extends Request {
     totalTracks?: number;
     cover?: string;
     collaborators?: { role: string; name: string }[];
-    category?: AudioCategory; // Nova categoria
+    category?: AudioCategory;
   };
   file?: Express.Multer.File;
 }
@@ -158,7 +174,7 @@ app.post('/upload', upload.single('audioFile'), async (req: UploadRequest, res: 
       title,
       description,
       artist,
-      artistAvatar, // Adiciona o avatar do artista
+      artistAvatar,
       album,
       genre,
       duration,
@@ -170,26 +186,13 @@ app.post('/upload', upload.single('audioFile'), async (req: UploadRequest, res: 
       totalTracks,
       cover,
       collaborators,
-      category = AudioCategory.OTHER, // Categoria padrão
+      category = AudioCategory.OTHER,
     } = req.body;
-    
 
-    if (!title || !artist || !duration || !artistAvatar) { // Verifica o avatar do artista
+    if (!title || !artist || !duration || !artistAvatar) {
       res.status(400).send('Título, Artista, Avatar e Duração são obrigatórios.');
       return;
     }
-
-    const audioDescription: string = description || '';
-    const audioAlbum: string = album || '';
-    const audioGenre: string = genre || '';
-    const audioCover: string = cover || '';
-    const audioCopyright: string = copyright || '';
-    const audioReleaseDate: Date = releaseDate ? new Date(releaseDate) : new Date();
-    const audioProducer: string = producer || '';
-    const audioComposer: string = composer || '';
-    const audioTrackNumber: number = trackNumber || 1;
-    const audioTotalTracks: number = totalTracks || 1;
-    const audioCollaborators = collaborators || [];
 
     const audioBuffer = req.file?.buffer;
 
@@ -205,50 +208,25 @@ app.post('/upload', upload.single('audioFile'), async (req: UploadRequest, res: 
 
     const audio: IAudio = new Audio({
       title,
-      description: audioDescription,
+      description,
       artist,
-      artistAvatar, // Salvar o avatar do artista
-      album: audioAlbum,
-      genre: audioGenre,
+      artistAvatar,
+      album,
+      genre,
       duration,
-      releaseDate: audioReleaseDate,
-      copyright: audioCopyright,
-      producer: audioProducer,
-      composer: audioComposer,
-      trackNumber: audioTrackNumber,
-      totalTracks: audioTotalTracks,
-      collaborators: [{
-        role: 'Producer',
-        name: 'SavanaPoint'
-      }],
+      releaseDate: releaseDate ? new Date(releaseDate) : new Date(),
+      copyright,
+      producer,
+      composer,
+      trackNumber: trackNumber || 1,
+      totalTracks: totalTracks || 1,
+      collaborators: collaborators || [],
       url,
-      cover: audioCover,
-      category, // Adicionar categoria
+      cover,
+      category,
     });
 
     await audio.save();
-
-    // const audioRef = push(dbRef(db, 'audios'));
-    // await set(audioRef, {
-    //   id: audio._id,
-    //   title: audio.title,
-    //   artist: audio.artist,
-    //   artistAvatar: audio.artistAvatar, // Avatar do artista no Firebase
-    //   album: audio.album,
-    //   genre: audio.genre,
-    //   duration: audio.duration,
-    //   releaseDate: audio.releaseDate,
-    //   copyright: audio.copyright,
-    //   producer: audio.producer,
-    //   composer: audio.composer,
-    //   trackNumber: audio.trackNumber,
-    //   totalTracks: audio.totalTracks,
-    //   collaborators: audio.collaborators,
-    //   url: audio.url,
-    //   createdAt: audio.createdAt,
-    //   cover: audio.cover,
-    //   category: audio.category, // Adicionar categoria no Firebase
-    // });
 
     res.status(201).json({ message: 'Áudio enviado com sucesso!', audio });
   } catch (error) {
@@ -258,6 +236,28 @@ app.post('/upload', upload.single('audioFile'), async (req: UploadRequest, res: 
       console.error('Erro ao processar o upload:', error);
       res.status(500).send('Erro ao processar o upload.');
     }
+  }
+});
+
+// Rota para incrementar a contagem de plays
+app.post('/audios/:id/play', async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { id } = req.params;
+
+    const audio = await Audio.findById(id);
+    if (!audio) {
+      res.status(404).json({ message: 'Áudio não encontrado' });
+      return;
+    }
+
+    // Incrementa a contagem de plays
+    audio.playCount += 1;
+    await audio.save();
+
+    res.status(200).json({ message: 'Contagem de plays atualizada', playCount: audio.playCount });
+  } catch (error) {
+    console.error('Erro ao atualizar a contagem de plays:', error);
+    res.status(500).json({ message: 'Erro ao atualizar a contagem de plays' });
   }
 });
 
@@ -272,12 +272,13 @@ app.get('/audios', async (req: Request, res: Response): Promise<void> => {
     if (artist) filter.artist = artist;
     if (genre) filter.genre = genre;
     if (album) filter.album = album;
-    if (category) filter.category = category; // Filtro de categoria
+    if (category) filter.category = category;
 
     const audios: IAudio[] = await Audio.find(filter)
       .sort({ createdAt: -1 })
       .skip((page - 1) * limit)
-      .limit(limit);
+      .limit(limit)
+      .select('title artist album genre playCount');
 
     const total = await Audio.countDocuments(filter);
 
@@ -290,6 +291,123 @@ app.get('/audios', async (req: Request, res: Response): Promise<void> => {
   } catch (error) {
     console.error('Erro ao obter as músicas:', error);
     res.status(500).send('Erro ao obter as músicas.');
+  }
+});
+
+// Rota para criar um álbum
+app.post('/albums', authenticateUser, async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { title, genre, artistAvatar, releaseDate, cover, trackIds } = req.body;
+
+    if (!title || !genre || !artistAvatar || !trackIds || trackIds.length === 0) {
+      res.status(400).json({ message: 'Título, gênero, avatar do artista e pelo menos uma música são obrigatórios.' });
+      return;
+    }
+
+    const tracks = await Audio.find({ _id: { $in: trackIds } });
+    if (tracks.length !== trackIds.length) {
+      res.status(404).json({ message: 'Uma ou mais músicas não foram encontradas.' });
+      return;
+    }
+
+    const album = new Album({
+      title,
+      artist: req.userId,
+      artistAvatar,
+      genre,
+      releaseDate: releaseDate ? new Date(releaseDate) : new Date(),
+      cover,
+      tracks: trackIds,
+    });
+
+    await album.save();
+
+    res.status(201).json({ message: 'Álbum criado com sucesso!', album });
+  } catch (error) {
+    console.error('Erro ao criar álbum:', error);
+    res.status(500).json({ message: 'Erro ao criar o álbum.' });
+  }
+});
+
+// Rota para listar álbuns de um artista
+app.get('/albums', async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { artist } = req.query;
+
+    const filter: any = {};
+    if (artist) filter.artist = artist;
+
+    const albums = await Album.find(filter).populate('tracks', 'title artist duration');
+
+    res.status(200).json({ albums });
+  } catch (error) {
+    console.error('Erro ao obter os álbuns:', error);
+    res.status(500).json({ message: 'Erro ao obter os álbuns.' });
+  }
+});
+
+// Rota para atualizar um álbum
+app.put('/albums/:id', authenticateUser, async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { id } = req.params;
+    const { title, genre, releaseDate, cover, trackIds } = req.body;
+
+    const album = await Album.findById(id);
+    if (!album) {
+      res.status(404).json({ message: 'Álbum não encontrado' });
+      return;
+    }
+
+    if (album.artist !== req.userId) {
+      res.status(403).json({ message: 'Você não tem permissão para atualizar este álbum.' });
+      return;
+    }
+
+    if (trackIds) {
+      const tracks = await Audio.find({ _id: { $in: trackIds } });
+      if (tracks.length !== trackIds.length) {
+        res.status(404).json({ message: 'Uma ou mais músicas não foram encontradas.' });
+        return;
+      }
+      album.tracks = trackIds;
+    }
+
+    album.title = title || album.title;
+    album.genre = genre || album.genre;
+    album.releaseDate = releaseDate ? new Date(releaseDate) : album.releaseDate;
+    album.cover = cover || album.cover;
+
+    await album.save();
+
+    res.status(200).json({ message: 'Álbum atualizado com sucesso!', album });
+  } catch (error) {
+    console.error('Erro ao atualizar o álbum:', error);
+    res.status(500).json({ message: 'Erro ao atualizar o álbum.' });
+  }
+});
+
+// Rota para excluir um álbum
+app.delete('/albums/:id', authenticateUser, async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { id } = req.params;
+
+    const album = await Album.findById(id);
+    if (!album) {
+      res.status(404).json({ message: 'Álbum não encontrado' });
+      return;
+    }
+
+    if (album.artist !== req.userId) {
+      res.status(403).json({ message: 'Você não tem permissão para excluir este álbum.' });
+      return;
+    }
+
+    await album.deleteOne();
+
+    res.status(200).json({ message: 'Álbum excluído com sucesso!' });
+  } catch (error) {
+    console.error('Erro ao excluir o álbum:', error);
+    res.status(500).json({ message: 'Erro ao excluir o álbum.' });
   }
 });
 
